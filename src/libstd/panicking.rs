998d9006dc6ce66bcdb1bfe10ab74194932500fe
roblabla <unfiltered@roblab.la>
2018-05-13 01:24:53 +0200

    Make panic allocation-less

--- src/libstd/panicking.rs
+++ src/libstd/panicking.rs
@@ -344,9 +344,17 @@ pub fn begin_panic_fmt(msg: &fmt::Arguments,
     // panic + OOM properly anyway (see comment in begin_panic
     // below).
 
-    let mut s = String::new();
-    let _ = s.write_fmt(*msg);
-    rust_panic_with_hook(Box::new(s), Some(msg), file_line_col)
+    //let mut s = String::new();
+    //let _ = s.write_fmt(*msg);
+    
+    //rust_panic_with_hook(Box::new(s), Some(msg), file_line_col)
+    // We panic'd, locks might already be taken. Let's avoid infinite looping there.
+    let _ = writeln!(::megaton_hammer::loader::Logger, "PANIC: {} in {}:{}:{}", msg, file_line_col.0, file_line_col.1, file_line_col.2);
+    
+    // TODO: Exit the program. Turns out this is surprisingly difficult.
+    // NOTE: This will not unwind the stack. If you panic, we'll almost
+    // certainly leak resources.
+    ::megaton_hammer::loader::exit(1)
 }
 
 /// This is the entry point of panicking for panic!() and assert!().
